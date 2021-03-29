36af2ab6af801db6eb4c885388e9b558f7293624
Karl Golka <contact@karlgolka.com>
2021-03-29 16:22:46 -0700

    feat: FE: react-ui InputField has a storybook and that story is being tested by cypress

--- apps/react-ui-e2e/src/integration/InputField/InputField.spec.ts
+++ apps/react-ui-e2e/src/integration/InputField/InputField.spec.ts
@@ -1,7 +1,7 @@
 describe('react-ui: InputField component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=inputfield--withLabel&knob-label=Success'));
+  beforeEach(() => cy.visit('/iframe.html?id=inputfield--primary&knob-label=Success'));
     
     it('should render the component', () => {
-      cy.get('input').should('contain', 'Success');
+      cy.get('body').should('contain', 'Success');
     });
 });
