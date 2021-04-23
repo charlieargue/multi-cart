3d2660702d3bfc9c9aeeefa43a4e2e5228752544
Karl Golka <contact@karlgolka.com>
2021-04-23 15:50:06 -0700

    feat: vercel troubleshooting

--- apps/multi-cart/appViews/LandingContainer.tsx
+++ apps/multi-cart/appViews/LandingContainer.tsx
@@ -54,7 +54,7 @@ export const LandingContainer = () => {
                         </Feature>
                     <Feature title="Always up to date" icon={<FcTimeline />}>
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                        invidunt ut labore. no public directory
+                        invidunt ut labore. no public directory... one more try
                         </Feature>
                     <Feature title="Incredible statistics" icon={<FcDoughnutChart />}>
                         At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
