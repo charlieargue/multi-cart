0b4aa1175d467268fc1ed44ea625cf8d3c25fc1d
Karl Golka <contact@karlgolka.com>
2021-04-27 17:42:35 -0700

    feat: adding variables file, formatting

--- libs/multi-cart-iac/tf-datasource.none.tf
+++ libs/multi-cart-iac/tf-datasource.none.tf
@@ -1,9 +1,9 @@
 
 # NONE DATASOURCE
 resource "aws_appsync_datasource" "none_datasource" {
-  api_id           = aws_appsync_graphql_api.MultiCartPOC.id
-  name             = "none_datasource"
-  type             = "NONE"
+  api_id = aws_appsync_graphql_api.MultiCartPOC.id
+  name   = "none_datasource"
+  type   = "NONE"
   depends_on = [
     aws_appsync_graphql_api.MultiCartPOC,
   ]
--- libs/multi-cart-iac/tf-resolvers-pipeline-forgot-password.tf
+++ libs/multi-cart-iac/tf-resolvers-pipeline-forgot-password.tf
@@ -1,31 +1,31 @@
-        #✅forgotPassword(email: String!): Boolean!
-        #✅ const user = await User.findOne({ where: { email } });
+#✅forgotPassword(email: String!): Boolean!
+#✅ const user = await User.findOne({ where: { email } });
 
 
-      # TODO: this needs to go as the 2nd STEP/FUNCTION
-        # EG: like the request-mapping for the 2nd step SESSION stuff would check if it has a user, etc...
-        # if (!user) {
-        #     // email is not in DB
-        #     //... 🛡 but for security reasons...
-        #     //    🛡 maybe just return true so they can't sniff for emails and PHISH!
-        #     return true; // or false... do nothing
-        # }
-        # + SESSIN STUFF:
-        # // TODO: RIP out old redis library and just need io-redis?
-        # const token = v4(); // guid
-        # // use redis to store these tokens
-        # await redis.set(FORGET_PASSWORD_PREFIX + token, user.id, 'ex', 1000 * 60 * 60 * 24 * 3); // 3 days validity
+# TODO: this needs to go as the 2nd STEP/FUNCTION
+# EG: like the request-mapping for the 2nd step SESSION stuff would check if it has a user, etc...
+# if (!user) {
+#     // email is not in DB
+#     //... 🛡 but for security reasons...
+#     //    🛡 maybe just return true so they can't sniff for emails and PHISH!
+#     return true; // or false... do nothing
+# }
+# + SESSIN STUFF:
+# // TODO: RIP out old redis library and just need io-redis?
+# const token = v4(); // guid
+# // use redis to store these tokens
+# await redis.set(FORGET_PASSWORD_PREFIX + token, user.id, 'ex', 1000 * 60 * 60 * 24 * 3); // 3 days validity
 
 
-      # AND THE 3rd FUNCTION
-        # // send them a special LINK w/ token
-        # await sendEmail(
-        #     email,
-        #     `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
-        # );
-        # return true;
+# AND THE 3rd FUNCTION
+# // send them a special LINK w/ token
+# await sendEmail(
+#     email,
+#     `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
+# );
+# return true;
 
-        ## GAMEPLAN:
+## GAMEPLAN:
 ## -------------------------------------
 ## 1) A - find user EXISTING FUNCTION, if not exist ERROR, if exist, pass to B
 
@@ -41,7 +41,7 @@ resource "aws_appsync_resolver" "forgot_password_pipeline_resolver" {
   response_template = file("./iac/AppSync/resolvers/user-resolvers/forgotPassword/pipeline/response-mapping.vtl")
   pipeline_config {
     functions = [
-      "${aws_appsync_function.get_user_function.function_id}", # A
+      "${aws_appsync_function.get_user_function.function_id}",                  # A
       "${aws_appsync_function.lambda_send_email_appsync_function.function_id}", # B
     ]
   }
