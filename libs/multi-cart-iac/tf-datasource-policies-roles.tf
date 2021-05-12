aa1f0faa3e4de6eb69cb99045d90479425fbb0f8
Karl Golka <contact@karlgolka.com>
2021-05-12 11:42:48 -0700

    feat: Extracting in-line policy into .json file

--- libs/multi-cart-iac/tf-datasource-policies-roles.tf
+++ libs/multi-cart-iac/tf-datasource-policies-roles.tf
@@ -44,23 +44,7 @@ resource "aws_iam_policy" "lambda_logging" {
   name        = "lambda_logging"
   path        = "/"
   description = "IAM policy for logging from a lambda"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
+  policy      = file("./AppSync/policies-roles/CloudWatch/policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
