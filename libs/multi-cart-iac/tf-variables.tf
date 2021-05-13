bdab798fe13a3207dad906ff4f5f48bc6e0d9824
Karl Golka <contact@karlgolka.com>
2021-05-12 17:57:45 -0700

    chore: testing tf cloud and VCS integration

--- libs/multi-cart-iac/tf-variables.tf
+++ libs/multi-cart-iac/tf-variables.tf
@@ -8,7 +8,8 @@ variable "aws_region" { default = "" }
 # LOCALS
 ##################################################################################
 locals {
-  env_name                     = lower(terraform.workspace) # a nice lowercase version, in case I ever switch actual workspace names
+  env_name                     = "dev" # 🔴 HARD CODED TEMP!
+  # env_name                     = lower(terraform.workspace) # a nice lowercase version, in case I ever switch actual workspace names
   assert_not_default_workspace = terraform.workspace == "default" ? file("ERROR: default workspace not allowed") : null
 
   common_tags = {
