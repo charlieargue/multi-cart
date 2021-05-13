ca51d04cd6e7e2f34fc78b766720aa33b81c7f98
Karl Golka <contact@karlgolka.com>
2021-05-13 14:46:42 -0700

    feat: why is one github action NOT triggering the other one

--- libs/multi-cart-iac/tf-main.tf
+++ libs/multi-cart-iac/tf-main.tf
@@ -28,7 +28,7 @@ provider "aws" {
 
 resource "aws_budgets_budget" "cost" {
   budget_type       = "COST"
-  limit_amount      = "100"
+  limit_amount      = "90"
   limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2017-07-01_00:00"
