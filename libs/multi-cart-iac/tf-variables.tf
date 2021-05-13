3e511ca4a6cb4d4fbf92e61ae4d870e7aa99ede1
Karl Golka <contact@karlgolka.com>
2021-05-12 18:11:36 -0700

    chore: CONTD working on tf cloud setup/issues

--- libs/multi-cart-iac/tf-variables.tf
+++ libs/multi-cart-iac/tf-variables.tf
@@ -11,7 +11,7 @@ variable "ORGANIZATION_NAME" { default = "" }
 locals {
   env_name                     = "dev" # 🔴 HARD CODED TEMP!
   # env_name                     = lower(terraform.workspace) # a nice lowercase version, in case I ever switch actual workspace names
-  assert_not_default_workspace = terraform.workspace == "default" ? file("ERROR: default workspace not allowed") : null
+  # assert_not_default_workspace = terraform.workspace == "default" ? file("ERROR: default workspace not allowed") : null
 
   common_tags = {
     OrgName     = "multi-cart"
