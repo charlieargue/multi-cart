e20f4ed196a455b53cc7cb38ca20295038896997
Karl Golka <contact@karlgolka.com>
2021-05-14 16:41:19 -0700

    iac: troubleshooting COGNITO contd

--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -8,12 +8,12 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   authentication_type = "API_KEY"
   additional_authentication_provider {
     authentication_type = "AMAZON_COGNITO_USER_POOLS"
-  }
-  # USER AUTHENTICATION is done with this:
-  user_pool_config {
-    aws_region     = var.AWS_REGION
-    # default_action = "ALLOW"
-    user_pool_id   = aws_cognito_user_pool.multicart_app_user_pool.id
+    # USER AUTHENTICATION is done with this:
+    user_pool_config {
+      aws_region = var.AWS_REGION
+      # default_action = "ALLOW"
+      user_pool_id = aws_cognito_user_pool.multicart_app_user_pool.id
+    }
   }
 
   log_config {
