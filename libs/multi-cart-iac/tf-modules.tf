558e37916c3b943312cde28685c18839bc8248b0
Karl Golka <contact@karlgolka.com>
2021-05-19 16:00:22 -0700

    iac: still ironing out variable naming conventions

--- libs/multi-cart-iac/tf-modules.tf
+++ libs/multi-cart-iac/tf-modules.tf
@@ -7,7 +7,7 @@
 module "send_email" {
   #   inputs:
   role_arn   = aws_iam_role.iam_role_for_lambda.arn
-  AWS_REGION = var.AWS_REGION
+  aws_region = var.AWS_REGION
   app_id     = aws_appsync_graphql_api.MultiCart.id
   common_tags = local.common_tags
   lambda_request_vtl = "./AppSync/functions/lambdaSendEmail/request-mapping.vtl"
