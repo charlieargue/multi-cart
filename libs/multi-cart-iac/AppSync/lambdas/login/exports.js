4aec7865ddf0b404ebea50d1c785e06ee4200c3a
Karl Golka <contact@karlgolka.com>
2021-05-19 21:52:00 -0700

    iac: successfully retrieving access token

--- libs/multi-cart-iac/AppSync/lambdas/login/exports.js
+++ libs/multi-cart-iac/AppSync/lambdas/login/exports.js
@@ -6,21 +6,12 @@ const {
 const {
     AWS_REGION_VAR
 } = process.env;
-
 const CognitoIdentityServiceProvider = require('aws-sdk/clients/cognitoidentityserviceprovider') // Much smaller size
 const AmazonCognitoIdentity = new CognitoIdentityServiceProvider({
     apiVersion: '2016-04-18',
     region: AWS_REGION_VAR
-
-    // DO I NEED?
-    // accessKeyId:...
-    // secretAccessKey: ...
 });
 
-
-
-// no-thx: https://docs.aws.amazon.com/cognito/latest/developerguide/authentication.html
-// no-thx: https://dzone.com/articles/authentication-and-authorization-to-amazon-cognito
 // thx: https://stackoverflow.com/questions/48313291/using-aws-cognito-in-a-lambda-function-with-npm
 // thx: https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-authentication-flow.html#amazon-cognito-user-pools-server-side-authentication-flow
 // big-thx: https://dev.to/franzwong/howto-implement-user-sign-up-and-login-with-aws-cognito-boa
@@ -65,6 +56,9 @@ exports.handler = async (event, context, callback) => {
             "Session": "string"
             }
         */
+       return {
+           token: data.AuthenticationResult.AccessToken
+       }
     } catch (err) {
         console.log(err, err.stack);
     }
