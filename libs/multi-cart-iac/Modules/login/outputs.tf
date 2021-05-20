ef11afa7051915f6e7f112e8410d17a4983f77e2
Karl Golka <contact@karlgolka.com>
2021-05-19 17:00:23 -0700

    iac: missing output added

--- libs/multi-cart-iac/Modules/login/outputs.tf
+++ libs/multi-cart-iac/Modules/login/outputs.tf
@@ -1,3 +1,6 @@
 output "function_arn" {
     value = aws_lambda_function.lambda_login_function.arn
 }
+output "function_id" {
+    value = aws_appsync_function.lambda_login_appsync_function.function_id
+}
