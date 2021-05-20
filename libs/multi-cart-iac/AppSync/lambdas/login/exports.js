4fa5fe59cf1d43dfc1390c356727d52223f4375e
Karl Golka <contact@karlgolka.com>
2021-05-19 21:10:37 -0700

    iac: switching to adminInitiateAuth

--- libs/multi-cart-iac/AppSync/lambdas/login/exports.js
+++ libs/multi-cart-iac/AppSync/lambdas/login/exports.js
@@ -22,53 +22,90 @@ const AmazonCognitoIdentity = new CognitoIdentityServiceProvider({
 // thx: https://docs.aws.amazon.com/cognito/latest/developerguide/authentication.html
 // thx: https://dzone.com/articles/authentication-and-authorization-to-amazon-cognito
 // thx: https://stackoverflow.com/questions/48313291/using-aws-cognito-in-a-lambda-function-with-npm
+// thx: https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-authentication-flow.html#amazon-cognito-user-pools-server-side-authentication-flow
 exports.handler = async (event, context, callback) => {
     const usernameOrEmail = event.usernameOrEmail
     const password = event.password
     console.log("👍👍👍👍👍👍👍👍👍👍👍 ~ event", event) // event has PAYLOAD, and we put whatever we want in there
     console.log(`🚀 ~ password`, password);
     console.log(`🚀 ~ usernameOrEmail`, usernameOrEmail);
+    
+    AmazonCognitoIdentity.adminInitiateAuth({
+        AuthFlow: 'ADMIN_NO_SRP_AUTH',
+        ClientId: CLIENT_ID,
+        UserPoolId: POOL_ID,
+        AuthParameters: {
+            USERNAME: usernameOrEmail,
+            PASSWORD: password,
+        },
+    }, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        }
+        else {
+            console.log("✅ ✅ ✅ ✅ ✅ ✅ ✅  ~ GREAT SUCCESS!");
+            console.log(data);
+        }
+    });
 
 
+    /*
+
+For server-side apps, user pool authentication is similar to that for client-side apps, except for the following:
+
+1) The server-side app calls the 🔥 AdminInitiateAuth API operation (instead of InitiateAuth). 
+- This operation requires AWS admin credentials. This operation returns the authentication parameters.
+
+2) Once it has the authentication parameters, the app calls the 🔥  AdminRespondToAuthChallenge API operation (instead of RespondToAuthChallenge), 
+- which also requires AWS admin credentials.
+
+3) The AdminInitiateAuth and AdminRespondToAuthChallenge operations can't accept user-name-and-password user credentials for admin sign-in, 
+- 🔥 unless you explicitly enable them to do so by doing one of the following:
+
+    a) Pass 🔥 ADMIN_USER_PASSWORD_AUTH (formerly known as ADMIN_NO_SRP_AUTH) for the ExplicitAuthFlow parameter in your server-side app's call to CreateUserPoolClient or UpdateUserPoolClient.
+    b) Choose Enable sign-in API for server-based authentication (🔥 ADMIN_USER_PASSWORD_AUTH) in the App clients tab in Create a user pool. 
+    - For more information, see Configuring a User Pool App Client.
+
+    */
 
     //   await cognitoProvider.adminInitiateAuth(...)
 
 
     // Amazon Cognito creates a session which includes the id, access, and refresh tokens of an authenticated user.
-    const authenticationData = {
-        Username: usernameOrEmail,
-        Password: password,
-    };
-    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-    const poolData = {
-        UserPoolId: POOL_ID,
-        ClientId: CLIENT_ID
-    };
-    const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-    const userData = {
-        Username: USERNAME,
-        Pool: userPool
-    };
-    const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function (result) {
-            const accessToken = result.getAccessToken().getJwtToken();
-            console.log(`🚀 ~ accessToken`, accessToken);
-            return {
-                token: accessToken
-            };
+    // const authenticationData = {
+    //     Username: usernameOrEmail,
+    //     Password: password,
+    // };
+    // const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    // const poolData = {
+    //     UserPoolId: POOL_ID,
+    //     ClientId: CLIENT_ID
+    // };
+    // const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    // const userData = {
+    //     Username: USERNAME,
+    //     Pool: userPool
+    // };
+    // const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    // cognitoUser.authenticateUser(authenticationDetails, {
+    //     onSuccess: function (result) {
+    //         const accessToken = result.getAccessToken().getJwtToken();
+    //         console.log(`🚀 ~ accessToken`, accessToken);
+    //         return {
+    //             token: accessToken
+    //         };
 
-            // does NOT apply?
-            /* Use the idToken for Logins Map when Federating User Pools with identity pools
-             or when passing through an Authorization Header to an API Gateway Authorizer */
-            const idToken = result.idToken.jwtToken;
-        },
+    //         // does NOT apply?
+    //         /* Use the idToken for Logins Map when Federating User Pools with identity pools
+    //          or when passing through an Authorization Header to an API Gateway Authorizer */
+    //         const idToken = result.idToken.jwtToken;
+    //     },
 
-        onFailure: function (err) {
-            alert(err);
-        },
+    //     onFailure: function (err) {
+    //         alert(err);
+    //     },
 
-    });
+    // });
 
 }
 
