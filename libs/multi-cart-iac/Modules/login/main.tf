fae0c9bced6c64da4cc7b827bc609157a8e1a064
Karl Golka <contact@karlgolka.com>
2021-05-19 17:32:35 -0700

    iac: trouble-shooting one thing at a time

--- libs/multi-cart-iac/Modules/login/main.tf
+++ libs/multi-cart-iac/Modules/login/main.tf
@@ -5,39 +5,39 @@ data "archive_file" "lambda_login_archive" {
   output_path = "./build/${local.filename}"
 }
 
-# AWS LAMBDA ƛ FUNCTION
-resource "aws_lambda_function" "lambda_login_function" {
-  function_name    = "lambda_login_function_${var.common_tags.Environment}"
-  role             = var.role_arn
-  handler          = "exports.handler"
-  source_code_hash = data.archive_file.lambda_login_archive.output_base64sha256
-  filename         = "./build/${local.filename}"
-  runtime          = "nodejs12.x"
-  publish          = true
-  tags = merge(var.common_tags, {
-    Description = "AWS lambda function for logging in (against COGNITO user pool)"
-  })
-}
+# # AWS LAMBDA ƛ FUNCTION
+# resource "aws_lambda_function" "lambda_login_function" {
+#   function_name    = "lambda_login_function_${var.common_tags.Environment}"
+#   role             = var.role_arn
+#   handler          = "exports.handler"
+#   source_code_hash = data.archive_file.lambda_login_archive.output_base64sha256
+#   filename         = "./build/${local.filename}"
+#   runtime          = "nodejs12.x"
+#   publish          = true
+#   tags = merge(var.common_tags, {
+#     Description = "AWS lambda function for logging in (against COGNITO user pool)"
+#   })
+# }
 
 
-# LAMBDA ƛ DATASOURCE
-resource "aws_appsync_datasource" "lambda_login_datasource" {
-  api_id           = var.app_id
-  name             = "lambda_login_datasource"
-  service_role_arn = var.role_arn
-  type             = "AWS_LAMBDA"
-  lambda_config {
-    function_arn = aws_lambda_function.lambda_login_function.arn
-  }
-}
+# # LAMBDA ƛ DATASOURCE
+# resource "aws_appsync_datasource" "lambda_login_datasource" {
+#   api_id           = var.app_id
+#   name             = "lambda_login_datasource"
+#   service_role_arn = var.role_arn
+#   type             = "AWS_LAMBDA"
+#   lambda_config {
+#     function_arn = aws_lambda_function.lambda_login_function.arn
+#   }
+# }
 
-## LAMBDA AppSync function WRAPPER
-resource "aws_appsync_function" "lambda_login_appsync_function" {
-  api_id                    = var.app_id
-  data_source               = aws_appsync_datasource.lambda_login_datasource.name
-  name                      = "lambda_login_appsync_function"
-  request_mapping_template  = file(var.lambda_request_vtl)
-  response_mapping_template = file(var.lambda_response_vtl)
-}
+# ## LAMBDA AppSync function WRAPPER
+# resource "aws_appsync_function" "lambda_login_appsync_function" {
+#   api_id                    = var.app_id
+#   data_source               = aws_appsync_datasource.lambda_login_datasource.name
+#   name                      = "lambda_login_appsync_function"
+#   request_mapping_template  = file(var.lambda_request_vtl)
+#   response_mapping_template = file(var.lambda_response_vtl)
+# }
 
 
