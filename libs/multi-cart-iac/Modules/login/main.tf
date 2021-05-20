a38cd42b486bba520d64b37805c94e0d7048f4a3
Karl Golka <contact@karlgolka.com>
2021-05-19 19:55:11 -0700

    iac: nope, CULPRIT is aws_lambda_function

--- libs/multi-cart-iac/Modules/login/main.tf
+++ libs/multi-cart-iac/Modules/login/main.tf
@@ -6,30 +6,30 @@ data "archive_file" "lambda_login_archive" {
 }
 
 # AWS LAMBDA ƛ FUNCTION
-resource "aws_lambda_function" "lambda_login_function" {
-  function_name    = "lambda_login_function_${var.common_tags.Environment}"
-  role             = var.role_arn
-  handler          = "exports.handler"
-  source_code_hash = data.archive_file.lambda_login_archive.output_base64sha256
-  filename         = "./build/${local.filename}"
-  runtime          = "nodejs12.x"
-  publish          = true
-  tags = merge(var.common_tags, {
-    Description = "AWS lambda function for logging in (against COGNITO user pool)"
-  })
-}
+# resource "aws_lambda_function" "lambda_login_function" {
+#   function_name    = "lambda_login_function_${var.common_tags.Environment}"
+#   role             = var.role_arn
+#   handler          = "exports.handler"
+#   source_code_hash = data.archive_file.lambda_login_archive.output_base64sha256
+#   filename         = "./build/${local.filename}"
+#   runtime          = "nodejs12.x"
+#   publish          = true
+#   tags = merge(var.common_tags, {
+#     Description = "AWS lambda function for logging in (against COGNITO user pool)"
+#   })
+# }
 
 
-# LAMBDA ƛ DATASOURCE
-resource "aws_appsync_datasource" "lambda_login_datasource" {
-  api_id           = var.app_id
-  name             = "lambda_login_datasource"
-  service_role_arn = var.role_arn
-  type             = "AWS_LAMBDA"
-  lambda_config {
-    function_arn = aws_lambda_function.lambda_login_function.arn
-  }
-}
+# # LAMBDA ƛ DATASOURCE
+# resource "aws_appsync_datasource" "lambda_login_datasource" {
+#   api_id           = var.app_id
+#   name             = "lambda_login_datasource"
+#   service_role_arn = var.role_arn
+#   type             = "AWS_LAMBDA"
+#   lambda_config {
+#     function_arn = aws_lambda_function.lambda_login_function.arn
+#   }
+# }
 
 # ## LAMBDA AppSync function WRAPPER
 # resource "aws_appsync_function" "lambda_login_appsync_function" {
