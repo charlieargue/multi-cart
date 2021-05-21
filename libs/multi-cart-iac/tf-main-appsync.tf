fe062a8c10401cad84480fa9568d6ae164d7ad34
Karl Golka <contact@karlgolka.com>
2021-05-20 18:08:35 -0700

    iac: ME: keeping both mixed auths, but switching them around

--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -4,16 +4,19 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   name   = "MultiCart_${local.common_tags.Environment}"
   schema = file("./AppSync/schema/schema.gql")
 
-  # TODO: this is temp, for during-inital-development ONLY! switch to a client-pool via COGNITO-user-pools for CLIENT-APP AUTHENTICATION later!
-  authentication_type = "API_KEY"
 
   # USER AUTHENTICATION is done with this:
+  authentication_type = "AMAZON_COGNITO_USER_POOLS"
+  user_pool_config {
+    aws_region   = var.AWS_REGION
+    user_pool_id = aws_cognito_user_pool.multicart_app_user_pool.id
+    default_action = "ALLOW"
+  }
+
+  # TODO: this is temp, for during-inital-development ONLY! switch to a client-pool via COGNITO-user-pools for CLIENT-APP AUTHENTICATION later!
+  # ADDITIONAL 
   additional_authentication_provider {
-    authentication_type = "AMAZON_COGNITO_USER_POOLS"
-    user_pool_config {
-      aws_region   = var.AWS_REGION
-      user_pool_id = aws_cognito_user_pool.multicart_app_user_pool.id
-    }
+    authentication_type = "API_KEY"
   }
 
   log_config {
