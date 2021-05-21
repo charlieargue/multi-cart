c7d705fecaa1b4a54dc6d781706e982afe94975a
Karl Golka <contact@karlgolka.com>
2021-05-20 18:38:49 -0700

    iac: SWITHCED AUTH: trouble-shooting - 1

--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -8,8 +8,8 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   # USER AUTHENTICATION is done with this:
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   user_pool_config {
-    aws_region   = var.AWS_REGION
-    user_pool_id = aws_cognito_user_pool.multicart_app_user_pool.id
+    aws_region     = var.AWS_REGION
+    user_pool_id   = aws_cognito_user_pool.multicart_app_user_pool.id
     default_action = "ALLOW"
   }
 
