ce9839ce3dcc5fe648d717a67e871e1303fa7c1a
Karl Golka <contact@karlgolka.com>
2021-05-21 17:18:55 -0700

    iac: FIXING prod deployment issue only way I know how - to comment out the register pipeline entirely for one apply

--- libs/multi-cart-iac/tf-resolvers-pipeline-register.tf
+++ libs/multi-cart-iac/tf-resolvers-pipeline-register.tf
@@ -8,19 +8,19 @@
 
 
 # PIPELINE resolver
-resource "aws_appsync_resolver" "register_pipeline_resolver" {
-  kind              = "PIPELINE"
-  api_id            = aws_appsync_graphql_api.MultiCart.id
-  field             = "register"
-  type              = "Mutation"
-  request_template  = file("./AppSync/resolvers/_generic/generic-request-mapping-EMPTY.vtl")
-  response_template = file("./AppSync/resolvers/user-resolvers/register/pipeline/response-mapping.vtl")
-  pipeline_config {
-    functions = [
-      // 1) setup COGNITO user
-      // 2) PUTITEM into DDB (and return with token from step 1)
-      "${module.register.appsync_function_id}",
-      "${aws_appsync_function.add_user_function.function_id}",
-    ]
-  }
-}
+# resource "aws_appsync_resolver" "register_pipeline_resolver" {
+#   kind              = "PIPELINE"
+#   api_id            = aws_appsync_graphql_api.MultiCart.id
+#   field             = "register"
+#   type              = "Mutation"
+#   request_template  = file("./AppSync/resolvers/_generic/generic-request-mapping-EMPTY.vtl")
+#   response_template = file("./AppSync/resolvers/user-resolvers/register/pipeline/response-mapping.vtl")
+#   pipeline_config {
+#     functions = [
+#       // 1) setup COGNITO user
+#       // 2) PUTITEM into DDB (and return with token from step 1)
+#       "${module.register.appsync_function_id}",
+#       "${aws_appsync_function.add_user_function.function_id}",
+#     ]
+#   }
+# }
