11c553d7c89b06b9b32b2621da8fa1eb711e4295
Karl Golka <contact@karlgolka.com>
2021-05-29 11:43:37 -0700

    feat: Identity Pool creation trbl-shooting - 1

--- libs/multi-cart-iac/tf-COGNITO-identity-pool.tf
+++ libs/multi-cart-iac/tf-COGNITO-identity-pool.tf
@@ -4,16 +4,17 @@
 # thx: https://stackoverflow.com/questions/48174927/public-queries-and-mutations-no-authentication
 # thx: https://eddielh.medium.com/the-missing-guide-to-aws-amplify-public-and-private-access-for-authenticated-and-unauthenticated-2f192cc91391
 ## ------------------------------
- resource "aws_cognito_identity_pool" "multicart_app_identity_pool" {
-      identity_pool_name               = "${local.common_tags.AppPrefix}cognito_identity_pool_${local.common_tags.Environment}"
-      allow_unauthenticated_identities = true # this POOL is ONLY to allow client/web apps to access the PUBLIC ENDPOINTS
+resource "aws_cognito_identity_pool" "multicart_app_identity_pool" {
+  identity_pool_name               = "${local.common_tags.AppPrefix}cognito_identity_pool_${local.common_tags.Environment}"
+  allow_unauthenticated_identities = true # this POOL is ONLY to allow client/web apps to access the PUBLIC ENDPOINTS
 
-      cognito_identity_providers {
-        # TODO: not sure if I should use the same user_pool_client or create a new one? CONFUSION: creating a new one still ties it to the old user_pool, 
-        # or I would have to create a new user pool just for this identity pool? That kind of makes sense actually....
-        # where will the AppClient "users" go if there isn't another pool???
-           client_id               = aws_cognito_user_pool_client.multicart_app_user_pool_client.id
-           server_side_token_check = true
-      }
- }
+  cognito_identity_providers {
+    # TODO: not sure if I should use the same user_pool_client or create a new one? CONFUSION: creating a new one still ties it to the old user_pool, 
+    # or I would have to create a new user pool just for this identity pool? That kind of makes sense actually....
+    # where will the AppClient "users" go if there isn't another pool???
+    client_id               = aws_cognito_user_pool_client.multicart_app_user_pool_client.id
+    provider_name           = ""
+    server_side_token_check = true
+  }
+}
 
