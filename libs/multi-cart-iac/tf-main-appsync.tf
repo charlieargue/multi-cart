d19c7d7a44b28d9f65d8689a6c061134f842b503
Karl Golka <contact@karlgolka.com>
2021-05-29 12:23:33 -0700

    feat: Identity Pool roles and policies ✅ SUCCESS, activating mixed auth in appsync api cfg, schema changes to include @aws_iam

--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -3,13 +3,19 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   name   = "MultiCart_${local.common_tags.Environment}"
   schema = file("./AppSync/schema/schema.gql")
 
-  # USER AUTHENTICATION is done with this:
+  # USER AUTHENTICATION for all PRIVATE endpoints:
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   user_pool_config {
     aws_region     = var.AWS_REGION
     user_pool_id   = aws_cognito_user_pool.multicart_app_user_pool.id
     default_action = "ALLOW"
   }
+
+  # UN-AUTHENTICATED access for all PUBLIC endpoints:
+  additional_authentication_provider {
+    authentication_type = "AWS_IAM"
+  }
+
 
   log_config {
     cloudwatch_logs_role_arn = aws_iam_role.multicart_appsync_logging_role.arn
