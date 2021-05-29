ee1fb22f2516ff038ad853aa8ba0a2da31ca2d22
Karl Golka <contact@karlgolka.com>
2021-05-29 12:43:27 -0700

    feat: mixed auth COGNITO and IAM trbl-shooting - 2

--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -3,18 +3,22 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   name   = "MultiCart_${local.common_tags.Environment}"
   schema = file("./AppSync/schema/schema.gql")
 
-  # default UN-AUTHENTICATED access for all PUBLIC endpoints:
-  authentication_type = "AWS_IAM"
-
   # USER AUTHENTICATION for all PRIVATE endpoints:
-  additional_authentication_provider {
-    authentication_type = "AMAZON_COGNITO_USER_POOLS"
-    user_pool_config {
-      aws_region     = var.AWS_REGION
-      user_pool_id   = aws_cognito_user_pool.multicart_app_user_pool.id
-    }
+  authentication_type = "AMAZON_COGNITO_USER_POOLS"
+  user_pool_config {
+    aws_region     = var.AWS_REGION
+    user_pool_id   = aws_cognito_user_pool.multicart_app_user_pool.id
+    
+    # (Required only if Cognito is used as the default auth provider) 
+    # The action that you want your GraphQL API to take when a request that uses Amazon Cognito User Pool authentication 
+    # doesn't match the Amazon Cognito User Pool configuration. Valid: ALLOW and DENY
+    default_action = "DENY"
   }
 
+  # UN-AUTHENTICATED access for all PUBLIC endpoints:
+  additional_authentication_provider {
+    authentication_type = "AWS_IAM"
+  }
 
 
   log_config {
