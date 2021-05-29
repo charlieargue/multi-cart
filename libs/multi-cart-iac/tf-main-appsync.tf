dfea993412cf25cea0ef30ce54228e5ef72bc14c
Karl Golka <contact@karlgolka.com>
2021-05-29 14:02:16 -0700

    iac: TEMPORARY adding back API_KEY while sorting things out

--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -6,18 +6,24 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   # USER AUTHENTICATION for all PRIVATE endpoints:
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   user_pool_config {
-    aws_region     = var.AWS_REGION
-    user_pool_id   = aws_cognito_user_pool.multicart_app_user_pool.id
-    
+    aws_region   = var.AWS_REGION
+    user_pool_id = aws_cognito_user_pool.multicart_app_user_pool.id
+
     # (Required only if Cognito is used as the default auth provider) 
     # The action that you want your GraphQL API to take when a request that uses Amazon Cognito User Pool authentication 
     # doesn't match the Amazon Cognito User Pool configuration. Valid: ALLOW and DENY
-    default_action = "DENY"
+    # TODO: confusion on this setting and its effects, TBD...
+    default_action = "ALLOW"
   }
 
   # UN-AUTHENTICATED access for all PUBLIC endpoints:
   additional_authentication_provider {
     authentication_type = "AWS_IAM"
+  }
+  
+  # TEMPORARY while sorting things out
+  additional_authentication_provider {
+    authentication_type = "API_KEY"
   }
 
 
@@ -28,4 +34,8 @@ resource "aws_appsync_graphql_api" "MultiCart" {
   tags = merge(local.common_tags, {
     Description = "GraphQL AppSync API for MultiCart"
   })
+}
+
+resource "aws_appsync_api_key" "key" {
+  api_id  = aws_appsync_graphql_api.MultiCart.id
 }
