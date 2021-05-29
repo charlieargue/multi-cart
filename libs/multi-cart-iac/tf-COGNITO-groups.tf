33383c27d9adf65bf61fa5b70d8bbb9e69d8a8c5
Karl Golka <contact@karlgolka.com>
2021-05-28 18:17:27 -0700

    iac: no, not going to work as I expected, undoing this, leaving the public endpoints public, nothing replaces x-api-key

--- libs/multi-cart-iac/tf-COGNITO-groups.tf
+++ libs/multi-cart-iac/tf-COGNITO-groups.tf
@@ -1,4 +1,3 @@
-# TODO: perhaps a separate role for the AppClients group?
 ## ------------------------------
 resource "aws_iam_role" "multicart_app_user_group_role" {
   name               = "${local.common_tags.AppPrefix}cognito_user_group_role_${local.common_tags.Environment}"
@@ -18,13 +17,5 @@ resource "aws_cognito_user_group" "multicart_app_admin_group" {
   name         = "Admin"
   user_pool_id = aws_cognito_user_pool.multicart_app_user_pool.id
   description  = "Admin group for admin-only functions, like hydrating accounts (only select users)"
-  role_arn     = aws_iam_role.multicart_app_user_group_role.arn
-}
-
-## ------------------------------ ATTN: part of app_client POOL
-resource "aws_cognito_user_group" "multicart_app_app_client_group" {
-  name         = "AppClient"
-  user_pool_id = aws_cognito_user_pool.multicart_app_client_pool.id
-  description  = "AppClient group (allows applications/clients access the public API endpoints, like login/register/etc.)"
   role_arn     = aws_iam_role.multicart_app_user_group_role.arn
 }
--- libs/multi-cart-iac/tf-main-appsync.tf
+++ libs/multi-cart-iac/tf-main-appsync.tf
@@ -1,9 +1,7 @@
-# thx: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/appsync_graphql_api
 ## ------------------------------
 resource "aws_appsync_graphql_api" "MultiCart" {
   name   = "MultiCart_${local.common_tags.Environment}"
   schema = file("./AppSync/schema/schema.gql")
-
 
   # USER AUTHENTICATION is done with this:
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
@@ -13,21 +11,11 @@ resource "aws_appsync_graphql_api" "MultiCart" {
     default_action = "ALLOW"
   }
 
-  # TODO: this is temp, for during-inital-development ONLY! switch to a client-pool via COGNITO-user-pools for CLIENT-APP AUTHENTICATION later!
-  # ADDITIONAL 
-  additional_authentication_provider {
-    authentication_type = "API_KEY"
-  }
-
   log_config {
     cloudwatch_logs_role_arn = aws_iam_role.multicart_appsync_logging_role.arn
     field_log_level          = "ALL"
   }
   tags = merge(local.common_tags, {
-    Description = "GraphQL API for MultiCart"
+    Description = "GraphQL AppSync API for MultiCart"
   })
-}
-
-resource "aws_appsync_api_key" "api_key" {
-  api_id = aws_appsync_graphql_api.MultiCart.id
 }
