13ad9792ac25fce3995e3360b4f8e6f1bea4011d
Karl Golka <contact@karlgolka.com>
2021-06-11 17:46:59 -0700

    feat: cart line row AUTOSAVE is over-firing, trble-shooting debounce, fixed and working great

--- libs/react-shared-components/src/lib/auto-save/AutoSave.tsx
+++ libs/react-shared-components/src/lib/auto-save/AutoSave.tsx
@@ -4,56 +4,27 @@ import { useFormikContext } from 'formik';
 import debounce from 'just-debounce-it';
 import React, { useCallback, useEffect, useState } from 'react';
 
-// TODO: put this in it's own custom hook! see `react-debounce-over-rendering.ISSUE.md`
+// TODO: put this in it's own custom hook! see `react-debounce-over-rendering.ISSUE.md` does NOT need to be a component
 
 // thx: https://itnext.io/formik-introduction-autosave-react-19d4c15cfb90
 // thx: https://codesandbox.io/s/formik-autosave-example-wfcb6?file=/src/AutoSavingForm.tsx:124-242
 // -------------------
-export const AutoSave = ({ debounceMs = 500 }) => {
+export const AutoSave = ({ debounceMs = 1000 }) => {
   const formik = useFormikContext();
-  const [isSaved, setIsSaved] = useState(null);
-  const [slowerIsSubmitting, setSlowerIsSubmitting] = useState(false);
+  const [, setIsSaved] = useState(null);
 
-  // -------------------
-  const debouncedSubmit = useCallback(() => {
-    const unsubscribeDebounceSubmit = debounce(() => {
-      return formik.submitForm().then(() => setIsSaved(true));
-    }, debounceMs);
-    return unsubscribeDebounceSubmit();
-  },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [formik.submitForm, debounceMs],
+  const debouncedSubmit = useCallback(
+    debounce(
+      () =>
+        formik.submitForm().then(() => setIsSaved(true)),
+      debounceMs
+    ),
+    [debounceMs, formik.submitForm]
   );
 
-  // -------------------
   useEffect(() => {
     debouncedSubmit();
   }, [debouncedSubmit, formik.values]);
 
-
-  // ------------------- CULPRIT!
-  useEffect(() => {
-    // NOTE: don't forget to unsubscribe, or get ERROR: To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
-    if (!!formik.isSubmitting) {
-      setSlowerIsSubmitting(formik.isSubmitting);
-    } else {
-      const timeout = setTimeout(() => {
-        setSlowerIsSubmitting(formik.isSubmitting);
-      }, 1000);
-      return () => clearInterval(timeout);
-    }
-    // slow it down on the falses!
-  }, [formik.isSubmitting]);
-
-
-  // TODO: ✅ NO, use a global context, and dispatch an event, that shows some indicator (top of page LINE or 3 dot next to logo etc...)
-  // -------------------
-  return (<Box></Box>);
-  // <p className="text-success w-100 floatingSave">
-  //   {!!formik.isSubmitting
-  //     ? 'Saving...'
-  //     : isSaved
-  //       ? 'Saved!'
-  //       : null}
-  // </p>
+  return (<Box hidden></Box>);
 };
