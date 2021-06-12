87d6889a9a1869621e3ced00df493995c9d60158
Karl Golka <contact@karlgolka.com>
2021-06-12 12:35:58 -0700

    feat: so removing reducer from useIsAuth, apparently fine to have useRouter router in dep array on useEffect, so...

--- libs/react-shared-components/src/lib/auth/useIsAuth.ts
+++ libs/react-shared-components/src/lib/auth/useIsAuth.ts
@@ -1,28 +1,14 @@
 import { useMeQuery } from "@multi-cart/react-data-access";
 import { useRouter } from "next/router";
-import { useEffect, useReducer } from "react";
-
-type Action = { type: 'replace' }
+import { useEffect } from "react";
 
 export const useIsAuth = () => {
     const [{ data, fetching }] = useMeQuery();
     const router = useRouter();
-
-    // NOTE: using useReducer in order to NOT have entire router in effect dep array
-    const initialState = undefined;
-    const reducer = (state: string, action: Action) => {
-        if (action.type === "replace") {
-            router.replace("/login?next=" + router.asPath);
-            return state; //hacky? not really using this state (yet)
-        } else {
-            throw new Error();
-        }
-    }
-    const [, dispatch] = useReducer(reducer, initialState);
-
+    
     useEffect(() => {
         if (!fetching && !data?.me) {
-            dispatch({ type: "replace" });
+            router.replace("/login?next=" + router.asPath);
         }
-    }, [fetching, data,]);
+    }, [fetching, data.me, router]);
 };
