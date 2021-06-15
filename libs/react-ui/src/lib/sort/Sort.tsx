397ee7fc759fba5148fb5960f9a08978277bf68c
Karl Golka <contact@karlgolka.com>
2021-06-14 21:52:53 -0700

    feat: FIXING sorting on carts - wow really working nicely now, tested on description

--- libs/react-ui/src/lib/sort/Sort.tsx
+++ libs/react-ui/src/lib/sort/Sort.tsx
@@ -5,26 +5,32 @@ import './Sort.module.scss';
 
 /* eslint-disable-next-line */
 export interface SortProps {
-  children?: React.ReactNode;
-  keyWith?: string;
+  children: React.ReactNode;
+  childType: string;
   by?: any;
+  keyWith?: string;
 }
 
 // thx: https://stackoverflow.com/a/55996695/6200791
 // thx: https://frontarm.com/james-k-nelson/passing-data-props-children/
 // -------------------
-export const Sort: React.FC<SortProps> = ({ children, by, keyWith = "id" }) => {
+export const Sort: React.FC<SortProps> = ({ children, childType, by, keyWith = "id" }) => {
 
-  const compare = (a: any, b: any): any => {
+  const compare = (aRaw: any, bRaw: any): any => {
+    const a = aRaw.props[childType];
+    const b = bRaw.props[childType];
+
     // Compare function needed by the Sort component
     // you can access the relevant property like this a.props[by]
     // depending whether you are sorting by tilte or year, you can write a compare function here, 
     switch (by) {
+
       case "createdAt":
-        return b.props[by] > a.props[by] ? 1 : -1;
+        return b[by] > a[by] ? 1 : -1;
         break;
+
       case "description":
-        return a.props[by].localeCompare(b.props[by]);
+        return a[by].localeCompare(b[by]);
         break;
     }
   }
@@ -43,8 +49,8 @@ export const Sort: React.FC<SortProps> = ({ children, by, keyWith = "id" }) => {
     const elements: any[] = React.Children.toArray(children);
     const keyed: any[] = elements
       .sort(compare)
-      .map((child) => {
-        return React.cloneElement(child, { key: (child as any)[keyWith] });
+      .map((child, newIdx) => {
+        return React.cloneElement(child, { key: (child as any)[keyWith], idx: newIdx });
       });
     return (<>{keyed}</>);
   }
