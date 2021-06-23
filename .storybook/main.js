1f71738223d3a758674996dcb1e207f4c1def644
Karl Golka <contact@karlgolka.com>
2021-06-23 12:15:50 -0700

    chore: upgrading nx packages cuz storybook issues, adding storybook SASS support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,22 @@
+// thx: https://storybook.js.org/docs/react/configure/webpack#extending-storybooks-webpack-config
+const path = require('path');
+
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-knobs/register'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
