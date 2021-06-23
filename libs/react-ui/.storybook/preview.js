6dde5146cdaa5f3c26decb6262152c91d5f16175
Karl Golka <contact@karlgolka.com>
2021-06-23 13:25:09 -0700

    test: STORYBOOK: allowing circular dependencies for theme import into preview

--- libs/react-ui/.storybook/preview.js
+++ libs/react-ui/.storybook/preview.js
@@ -4,19 +4,19 @@ import { store } from '@multi-cart/react-app-state';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { withKnobs } from '@storybook/addon-knobs';
-// import { theme } from '@multi-cart/react-shared-components';
+import { theme } from '@multi-cart/react-shared-components';
 
-// addDecorator((storyFn) => (
-//     <ChakraProvider resetCSS theme={theme}>
-//         <ColorModeProvider
-//             options={{
-//                 useSystemColorMode: false,
-//             }}>
-//             <Provider store={store}>
-//                 {storyFn()}
-//             </Provider>
-//         </ColorModeProvider>
-//     </ChakraProvider>
-// ));
+addDecorator((storyFn) => (
+    <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+            options={{
+                useSystemColorMode: false,
+            }}>
+            <Provider store={store}>
+                {storyFn()}
+            </Provider>
+        </ColorModeProvider>
+    </ChakraProvider>
+));
 
 addDecorator(withKnobs);
