567b56e46de387e2a826280c24ca2659242402b4
Karl Golka <contact@karlgolka.com>
2021-06-25 02:58:36 -0700

    fix: found CULPRIT of circ. dep. issue: need to move styles.scss GLOBALS out into separate project (into shared-components, layout, right)

--- libs/react-ui/.storybook/preview.js
+++ libs/react-ui/.storybook/preview.js
@@ -3,7 +3,6 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { addDecorator } from '@storybook/react';
 import React from 'react';
 import { theme, Providers } from '@multi-cart/react-shared-components';
-// import '../../../apps/multi-cart/styles/styles.scss';
 
 addDecorator((storyFn) => (
     <ThemeProvider theme={theme}>
