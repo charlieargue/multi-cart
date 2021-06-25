d39ab4ad49774ab43d146ad22825928f3ba8bbf1
Karl Golka <contact@karlgolka.com>
2021-06-25 15:44:45 -0700

    fix: vercel typescript ONE MORE fix

--- apps/multi-cart/appViews/products/ProductsContainer.tsx
+++ apps/multi-cart/appViews/products/ProductsContainer.tsx
@@ -1,5 +1,5 @@
 import { Alert, AlertIcon, SimpleGrid, Text } from '@chakra-ui/react';
-import { AppLayout, ProductCard } from '@multi-cart/react-shared-components';
+import { AppLayout, Product, ProductCard } from '@multi-cart/react-shared-components';
 import { Breadcrumbs } from '@multi-cart/react-ui';
 import React from 'react';
 import { ProductsMockData as products } from '@multi-cart/mock-api';
@@ -24,7 +24,7 @@ export function ProductsContainer(props: ProductsContainerProps) {
         <Text>These are all <strong>mocked</strong> products <strong>&nbsp;and the images change</strong> because they're dynamically generated ... <span role='img' aria-label='emoji'>🛍</span> &nbsp;but you can still <strong>add them to your cart</strong>!</Text>
       </Alert>
       <SimpleGrid minChildWidth="320px" spacing="40px">
-        {products.map((product) => (
+        {products.map((product: Product) => (
           <ProductCard key={product.slug} product={product} />
         ))}
       </SimpleGrid>
