18b22b5fdddf202fed7998232f4aeaa07b81539d
Karl Golka <contact@karlgolka.com>
2021-06-30 14:55:23 -0700

    cicd: testing jest - 11, ... housekeeping getting rid of unused jest specs in react-shared-components

--- libs/react-shared-components/src/lib/cart/cart-name-editable/CartNameEditable.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-
-describe('CartNameEditable', () => {
-  it('should render successfully', () => {
-    // const { baseElement } = render(<CartNameEditable />);
-    // expect(baseElement).toBeTruthy();
-  });
-});
--- libs/react-shared-components/src/lib/hero/Hero.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Hero from './Hero';
-
-describe('Hero', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Hero />);
-    expect(baseElement).toBeTruthy();
-  });
-});
