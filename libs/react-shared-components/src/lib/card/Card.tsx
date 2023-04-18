742497727db615ad8701a6178effe4cf607fd3d5
Karl Golka <contact@karlgolka.com>
2023-04-18 15:24:40 -0700

    fix: chakra-ui errors from build (#23)
    • other cleanup along the way

--- libs/react-shared-components/src/lib/card/Card.tsx
+++ libs/react-shared-components/src/lib/card/Card.tsx
@@ -45,7 +45,6 @@ export function Card() {
             src={
               'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
             }
-            alt={'Author'}
             css={{
               border: '2px solid white',
             }}
--- libs/react-shared-components/src/lib/hero/Hero.tsx
+++ libs/react-shared-components/src/lib/hero/Hero.tsx
@@ -1,22 +1,16 @@
-// ##################################################################################
-// ℹ️ NOT READY YET or NOT MY CODE (chakra templates) ----- please ignore this file, thanks!
-// ##################################################################################
-
 import {
   Box,
   Button,
   Flex,
-  Heading, Image,
+  Heading,
+  Image,
   Link,
-
-  Stack
-} from "@chakra-ui/react";
+  Stack,
+} from '@chakra-ui/react';
 import { TextMuted } from '@multi-cart/react-ui';
 import NextLink from 'next/link';
-import React from "react";
+import React from 'react';
 import { FiExternalLink as ExternalLink } from 'react-icons/fi';
-import './Hero.module.scss';
-
 
 export interface HeroProps {
   title: string;
@@ -26,34 +20,33 @@ export interface HeroProps {
   ctaLink: string;
 }
 
-// -------------------
 export const Hero = ({
   title,
   subtitle,
   image,
   ctaLink,
   ctaText,
-  ...rest
+  ...props
 }: HeroProps) => {
   return (
     <Flex
       align="center"
-      justify={{ base: "center", md: "space-around", xl: "space-between" }}
-      direction={{ base: "column-reverse", md: "row" }}
+      justify={{ base: 'center', md: 'space-around', xl: 'space-between' }}
+      direction={{ base: 'column-reverse', md: 'row' }}
       px={8}
-      {...rest}
+      {...props}
     >
       <Stack
         spacing={4}
-        w={{ base: "80%", md: "40%" }}
-        align={["center", "center", "flex-start", "flex-start"]}
+        w={{ base: '80%', md: '40%' }}
+        align={['center', 'center', 'flex-start', 'flex-start']}
       >
         <Heading
           as="h1"
           size="xl"
           fontWeight="bold"
           color="primary.800"
-          textAlign={["center", "center", "left", "left"]}
+          textAlign={['center', 'center', 'left', 'left']}
         >
           {title}
         </Heading>
@@ -64,7 +57,7 @@ export const Hero = ({
           opacity="0.8"
           fontWeight="normal"
           lineHeight={1.5}
-          textAlign={["center", "center", "left", "left"]}
+          textAlign={['center', 'center', 'left', 'left']}
         >
           {subtitle}
         </Heading>
@@ -81,18 +74,24 @@ export const Hero = ({
           </Button>
         </NextLink>
         <TextMuted style={{ marginLeft: '16px' }}>
-          {/* TODO: make this an ExternalLink cmnpt like this: https://richardhaines.dev/chakra-external-link-with-svg-icon/ */}
-          Built by <Link href="http://karlgolka.com" isExternal mr="1">Karl Golka <ExternalLink style={{display: "inline"}} /></Link>in 2021
-          </TextMuted>
+          Built by{' '}
+          <Link href="http://karlgolka.com" isExternal mr="1">
+            Karl Golka <ExternalLink style={{ display: 'inline' }} />
+          </Link>
+          in 2021
+        </TextMuted>
       </Stack>
-      <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
-        <Image src={image} size="100%" rounded="1rem" shadow="2xl" fallbackSrc="/blue-loading-spinner-transparent-bg.gif" />
+      <Box w={{ base: '80%', sm: '60%', md: '50%' }} mb={{ base: 12, md: 0 }}>
+        <Image
+          src={image}
+          boxSize="100%"
+          rounded="1rem"
+          shadow="2xl"
+          fallbackSrc="/blue-loading-spinner-transparent-bg.gif"
+        />
       </Box>
     </Flex>
   );
-}
-
-
-
+};
 
 export default Hero;
